22f3feffed1369bb8b7e363361b9125b4732316a
Jakub Golinowski <jakubg@stud18-h57.inf.ethz.ch>
2017-11-30 15:10:33 +0100

    introduced B indices set

--- corsets.py
+++ corsets.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 num_dims = 250
+k = 5 # number of centers
 
-def compute_distance_to_closest_center(centers_vector, data_point):
+def compute_distance_to_closest_center(data_point, centers_vector):
     # centers_vector - 2d numpy array of shape (num_centers, num_dims)
     # data_poin - 1d numpy array of shape (num_dims)
     
@@ -22,12 +23,23 @@ def compute_distance_to_closest_center(centers_vector, data_point):
 def mapper(key, value):
     # key: None
     # value: 2d numpy array of shape (num_samples, num_dims) 
+    
+    num_data_points = value.shape[0]
 
     # idea: create a corset from the input data
     # step 1: D^2 sampling
     
-    D = [] #initialize D as empty set
-
+    D = np.zeros(k)
+    
+    #choose first cluster center uniformly at random
+    B_indices[0] = np.random.randint(A.shape[0], size=1)
+    #here I am not sure, for now choosing next point as point with highest "sensitivity" but the denomitor consists only of the points that in the 
+    
+    for center_index in range(1, k):
+        for data_point_index in range(num_data_points):
+            
+        
+    
 
 
     yield 0, "value"  # all mappers should yield the same key
@@ -44,5 +56,5 @@ if __name__ == "__main__":
     # testing the compute_distance_to_closest_center function
     centers_vector = np.array([[1.,1.],[2.,2.]])
     data_point = np.array([4.,4.])
-    distance_to_closest_center = compute_distance_to_closest_center(centers_vector, data_point)
+    distance_to_closest_center = compute_distance_to_closest_center(data_point, centers_vector)
     print(distance_to_closest_center)
